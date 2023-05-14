3746dc7421b4c1dff6a6fe48cedcf6d070ac0a33
vanande <101594646+vanande@users.noreply.github.com>
2023-05-14 19:56:03 +0100

    add missing files

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -34,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/Users/me/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/223.8617.54/bin/mingw/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/Users/me/AppData/Local/JetBrains/Toolbox/apps/CLion/ch-0/231.8770.66/bin/mingw/bin/objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
--- main.c
+++ main.c
@@ -7,34 +7,72 @@ size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
     return size * nmemb;
 }
 
-int main(void) {
+
+void showNum(char *s){
+    int i = 0;
+    while(s[i] != '\0'){
+        printf("\n%c = %d", s[i], s[i]);
+        i++;
+    }
+}
+
+char* replaceSpecialChar(char *str) {
+    int specialCharInt[] = {-126, -118, -123, -105}; // é, è, à, ù
+    char replaceCharInt[] = {101, 101, 97, 117}; // e, e, a, u
+
+    int i,j = 0;
+
+    while (str[i] != '\0') {
+        for (j = 0; j < 4; j++) {
+            if (str[i] == specialCharInt[j]) {
+                str[i] = replaceCharInt[j];
+            }
+        }
+        i++;
+    }
+    return str;
+}
+
+int main(int argc, char ** argv) {
     CURL *curl;
     CURLcode res;
     char *response = NULL;
     char question[40];
 
-    printf("\nType your question: ");
-    fgets(question, 40, stdin);
-    if (question[strlen(question) - 1] == '\n') {
-        question[strlen(question) - 1] = '\0';
-    }
 
-    //printf("The question is %s\n", question);
+    printf("Welcome to the FAQ system. Ask your question and we will try to answer it.\n");
+    do {
 
-    curl = curl_easy_init();
-    if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:9000/faq");
-        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, question);
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
 
-        res = curl_easy_perform(curl);
-
-        if (res != CURLE_OK) {
-            fprintf(stderr, "curl_easy_perform() failed: %s\n",
-                    curl_easy_strerror(res));
+        printf("\nType your question: ");
+        fgets(question, 40, stdin);
+        if (question[strlen(question) - 1] == '\n') {
+            question[strlen(question) - 1] = '\0';
         }
-        curl_easy_cleanup(curl);
-    }
+
+        //showNum(question);
+        //printf("\nQuestion old : %s", question);
+
+        strcpy(replaceSpecialChar(question), question);
+        //printf("\nQuestion new : %s", question);
+
+
+        curl = curl_easy_init();
+        if (curl) {
+            curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:9000/faq");
+            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, question);
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+
+            res = curl_easy_perform(curl);
+
+            if (res != CURLE_OK) {
+                fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                        curl_easy_strerror(res));
+            }
+            curl_easy_cleanup(curl);
+
+        }
+    } while (strcmp(question, "exit") != 0);
     return 0;
 }
